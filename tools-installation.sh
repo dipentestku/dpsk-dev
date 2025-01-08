45afbcde741cd6ee4b063dee81d2d4b15de7e4b4
dipentestku <dipentestku@gmail.com>
2025-01-08 12:46:12 +0700

    v1.0.0

---
+++ tools-installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../../tmp > /dev/null 2>&1 || { echo "Direktori 'temp' tidak ditemukan!"; exit 1; }
+
+echo -e "Sedang menginstall ${YELLOW}git${NC}"
+sudo apt-get install -y git >> /dev/null
+git clone https://github.com/dipentestku/dpsk-tool.git > /dev/null 2>&1 || { echo "Gagal meng-clone repository!"; exit 1; }
+cd dpsk-tool > /dev/null 2>&1 || { echo "Direktori 'dpsk-tool' tidak ditemukan!"; exit 1; }
+sudo python3 -m pip install . --break-system-packages > /dev/null 2>&1 || { echo "Gagal menginstal package!"; exit 1; }
+cd ~
+echo "Proses selesai dengan sukses!"
